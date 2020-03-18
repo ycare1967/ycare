0cab03d010b3c36c18a5ab7dd75c52686025bd6c
ycare1967 <54989160+ycare1967@users.noreply.github.com>
2020-03-18 20:10:04 +0100

    Add files via upload

---
+++ ST01_Homepage.spec.js
@@ -0,0 +1,17 @@
+
+    describe('Launch URL', function() {
+      
+        it('Verify user can login', function() {
+      
+          cy.visit('https://studio-dev.inits.xyz')
+
+
+           
+
+          })
+          after (function()  {
+            cy.clearCookie
+            });  
+        })
+        
+       
